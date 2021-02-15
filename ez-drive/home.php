e347fe5c966bdfb2828f16447c7dd937c67e45c5
alferdo.fadlillah17 <alferdo933@gmail.com>
2021-02-15 11:59:39 +0700

    edit home

--- ez-drive/home.php
+++ ez-drive/home.php
@@ -36,7 +36,7 @@ include 'Controller/koneksi.php';
         <!-- ============================================================== -->
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="index.html"><img src="logo/logo.png" width="135px" height="65px"></a>
+                <a class="navbar-brand" href="home.php"><img src="logo/logo.png" width="135px" height="65px"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -228,7 +228,7 @@ include 'Controller/koneksi.php';
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                             Copyright © 2021. All rights reserved Dandy Nizam Achmady
+                             Copyright © 2021. All rights reserved Alferdo,Dandy,Dinda,Firdatus.
                         </div>
                     </div>
                 </div>
